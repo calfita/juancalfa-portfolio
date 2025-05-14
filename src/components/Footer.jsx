15c33390cd443020a50616652ede6694848f84d9
calfmike <juan.calfa@hotmail.com>
2025-05-14 05:57:13 -0300

    mas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="footer">
       <p>
         Contacto: <a href="mailto:juan.calfa@hotmail.com">juan.calfa@hotmail.com</a> · 
-        <a href="https://github.com/calfmike" target="_blank"> GitHub</a> · 
+        <a href="https://github.com/calfita" target="_blank"> GitHub</a> · 
         <a href="https://linkedin.com/in/juan-ignacio-calfa-30a844127" target="_blank"> LinkedIn</a>
       </p>
     </footer>
